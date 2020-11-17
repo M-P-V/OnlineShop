77f4a93d7a3426d8cf15e1f43c49ce60ab98f0dc
M_P_V <mihail.pv.96@gmail.com>
2020-11-17 23:52:17 +0200

    Initial commit

---
+++ ProjectItems/SharedAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OnlineShop")]
+[assembly: AssemblyTrademark("")]
+
+
+[assembly: ComVisible(false)]
+[assembly: Guid("fbb8b454-9074-48f0-9514-af0fa8c8e83e")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
