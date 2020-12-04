772c711b580ed8dbce11330b584b55ebac0d97f2
M_P_V <mihail.pv.96@gmail.com>
2020-12-04 11:33:39 +0200

    Initial commit

--- OnlineShop.Web/Startup.cs
+++ OnlineShop.Web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OnlineShop.Domain;
 
 namespace OnlineShop.Web
 {
@@ -24,6 +26,10 @@ namespace OnlineShop.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<OnlineShopDbContext>(optionsBuilder =>
+            {
+                optionsBuilder.UseMySQL(Configuration.GetConnectionString("OnlineShop"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
